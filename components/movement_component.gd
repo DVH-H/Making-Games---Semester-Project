56d80398c531acc142302ff7bee856e9ce4d7dc8
Emil Fenger <fenger.emil@gmail.com>
2025-11-01 13:38:19 +0100

    changed the gun to take input from player script and added bullet types

--- components/movement_component.gd
+++ components/movement_component.gd
@@ -4,6 +4,9 @@ class_name MovementComponent
 var speed: int
 var jump_velocity: int
 
+var ground_acc: float = 6.0
+var ground_deacc: float = 8.0
+
 func set_speed(s: int):
 	speed = s
 	
@@ -18,4 +21,9 @@ func handle_jump(body: PhysicsBody2D) -> void:
 	
 func handle_movement(body: PhysicsBody2D, direction: Vector2) -> void:
 	body.velocity = direction * speed
+	
+func horizontal_movement_with_acc(body: PhysicsBody2D, direction: float) -> void:
+	var velocity_change: float = 0.0
+	velocity_change = ground_acc if direction != 0 else ground_deacc
+	body.velocity.x = move_toward(body.velocity.x, direction * speed, velocity_change)
 	
--- player/gun.gd
+++ player/gun.gd
@@ -1,29 +1,29 @@
 extends Node2D
 
-const BULLET = preload("res://Bullets/prefabs/bullet.tscn")
-@export var inputController: InputComponent
-var _direction: Vector2 = Vector2(1,0)
-
+const BULLET = preload("res://Bullets/prefabs/knockback_bullet.tscn")
 @onready var muzzle: Marker2D = $Marker2D
 
-func _process(delta: float) -> void:
+
+func shoot(direction):
+	var bullet_instance = BULLET.instantiate()
+	get_tree().root.add_child(bullet_instance)
+	bullet_instance.initialize(direction)
+	bullet_instance.global_position = muzzle.global_position
+	return bullet_instance.knockback_force
+
+func aim(dir):
 	#Mostly for debugging
 	var stick_dir = Vector2.ZERO
 	if len(Input.get_connected_joypads()) < 0:
 		var mouse_pos: Vector2 = get_global_mouse_position()
 		stick_dir = (mouse_pos - global_position).normalized()
 	else:
-		stick_dir = inputController.get_aim_input()
-	if stick_dir != Vector2.ZERO:
-		_direction = stick_dir
-	look_at(global_position + _direction)
+		stick_dir = dir
+	look_at(global_position + stick_dir)
 	rotation_degrees = wrap(rotation_degrees, 0, 360)
 	if rotation_degrees > 90 and rotation_degrees < 270:
 		scale.y = -1
 	else:
 		scale.y = 1
-	if Input.is_action_just_pressed("shoot"):
-		var bullet_instance = BULLET.instantiate()
-		get_tree().root.add_child(bullet_instance)
-		bullet_instance.initialize(_direction)
-		bullet_instance.global_position = muzzle.global_position
+	
+		
