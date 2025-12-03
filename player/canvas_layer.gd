e64d1bb8ee38dedd33fbe5b88610fddb80dbae33
dhel <dhel@itu.dk>
2025-12-03 17:30:16 +0100

    RevolverCylinderUi Now with images
    A lot of code cleanup and moved some sprites around.

--- player/canvas_layer.gd
+++ player/canvas_layer.gd
@@ -21,7 +21,6 @@ func _ready() -> void:
 		print("HUD: Synced wheel capacity to gun capacity: ", gun.capacity)
 
 	# --- Initial sync ---
-	_on_ammo_changed(gun.get_ammo_count())
 	_on_chamber_changed(gun.current_index)
 
 	# States from chambers
@@ -32,13 +31,17 @@ func _ready() -> void:
 
 	if gun.has_method("get_chamber_colors"):
 		_on_chamber_colors_updated(gun.get_chamber_colors())
+	
+	if gun.has_method("_icons_from"):
+		_on_chamber_icons_updated(gun._icons_from(gun.chambers))
 
 	# --- Signals -> UI ---
-	gun.ammo_changed.connect(_on_ammo_changed)
 	gun.chamber_changed.connect(_on_chamber_changed)
 	gun.chambers_updated.connect(_on_chambers_updated)
 	if gun.has_signal("chamber_colors_updated"):
 		gun.chamber_colors_updated.connect(_on_chamber_colors_updated)
+	if gun.has_signal("chamber_icons_updated"):
+		gun.chamber_icons_updated.connect(_on_chamber_icons_updated)
 	
 	# Connect player health signals
 	if player and player.has_signal("health_changed"):
@@ -49,9 +52,6 @@ func _ready() -> void:
 	else:
 		push_error("HUD: player node not found or doesn't have health_changed signal")
 
-func _on_ammo_changed(a: int) -> void:
-	wheel.set_ammo_count(a)
-
 func _on_chamber_changed(idx: int) -> void:
 	wheel.set_current_index(idx)
 
@@ -60,6 +60,9 @@ func _on_chambers_updated(states: Array[bool]) -> void:
 
 func _on_chamber_colors_updated(colors: Array[Color]) -> void:
 	wheel.set_chamber_colors(colors)
+
+func _on_chamber_icons_updated(icons: Array[Texture2D]) -> void:
+	wheel.set_chamber_icons(icons)
 
 func _on_health_changed(new_health: int, max_health_value: int) -> void:
 	if health_ui:
