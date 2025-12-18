4f02ceca669502ceec9d00b2a268d5faf3a060ad
Emil Fenger <fenger.emil@gmail.com>
2025-12-18 16:57:34 +0100

    Controls graphic added

--- Levels/comic_story.gd
+++ Levels/comic_story.gd
@@ -25,6 +25,7 @@ func _ready() -> void:
 		timer.timeout.connect(_on_timer)
 		Camera.position_smoothing_enabled = false
 		Camera.position = Vector2.ZERO
+		Menu.ui_disabled = true
 		get_tree().paused = true
 		for child in player.get_children():
 			if child.name == "CanvasLayer":
@@ -45,6 +46,7 @@ func _on_timer():
 		GameController.goto_scene(next_scene.resource_path)
 		get_tree().paused = false
 		Camera.position_smoothing_enabled = true
+		Menu.ui_disabled = false
 	else:
 		image_list[index].visible = true
 		timer.wait_time = timer_lengths[index]
--- Levels/menuUI.gd
+++ Levels/menuUI.gd
@@ -3,10 +3,11 @@ extends CanvasLayer
 @onready var play_button: TextureButton = $Control/MarginContainer/HBoxContainer/VBoxContainer/playButton
 @onready var controls_button: TextureButton = $Control/MarginContainer/HBoxContainer/VBoxContainer/ControlsButton
 @onready var quit_button: TextureButton = $Control/MarginContainer/HBoxContainer/VBoxContainer/quitButton
-
+@onready var controls: TextureRect = $Control/TextureRect
+var ui_disabled = false
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("reset"):
+	if Input.is_action_just_pressed("reset") and not ui_disabled:
 		get_tree().paused = not get_tree().paused
 		visible = not visible
 		
@@ -18,11 +19,12 @@ func _ready() -> void:
 	
 	
 func _press_play():
+	controls.visible = false
 	visible = false
 	get_tree().paused = false
 	
 func _press_controls():
-	pass
+	controls.visible = not controls.visible
 	
 func _press_quit():
 	get_tree().quit(0)
